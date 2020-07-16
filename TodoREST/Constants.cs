cb2da574bf5135e9b121f3306d50b1e9119820c3
Edwin Daniel Baltazar Terbullino <edwin.baltazar@tecsup.edu.pe>
2020-07-15 22:31:55 -0500

    Enviar cambios finales

--- TodoREST/Constants.cs
+++ TodoREST/Constants.cs
@@ -6,7 +6,7 @@ namespace TodoREST
     {
         // URL of REST service
         //public static string RestUrl = Device.RuntimePlatform == Device.Android ? "https://10.0.2.2:5001/api/todoitems/{0}" : "https://localhost:5001/api/todoitems/{0}";
-        public static string RestUrl = "http://192.168.1.5:3000/productos/{0}";
+        public static string RestUrl = "http://apirestxamarin.herokuapp.com/productos/{0}";
 
     }
 }
